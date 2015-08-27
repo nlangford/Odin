7fbc450892b997a799e421aa02452f0be57b68e6
Neil <neil.langford@live.com>
2015-08-27 12:15:24 +0100

    completed bubble sort part 2

--- bubble_sort/bubsort.rb
+++ bubble_sort/bubsort.rb
@@ -17,3 +17,18 @@ def bubble_sort(nums)
 end
 
 bubble_sort([1,2,3,4,5].shuffle)
+
+def bubble_sort_by(nums)
+  begin
+    swapped = false
+    (1...nums.length).each do |i|
+      if yield(nums[i-1], nums[i]) < 0
+        nums[i-1], nums[i] = nums[i], nums[i-1]
+        swapped = true
+      end
+    end
+  end while swapped
+  print nums
+end
+
+bubble_sort_by(["hi","hello","hey"]) { |left, right| left.length - right.length }
